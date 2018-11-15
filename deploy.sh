31a7873b04bc2e930ced0f371a81df2f4a091701
liaoqiao@corp.netease.com <liaoqiao@corp.netease.com>
2018-11-15 22:28:33 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-# cd dist
+cd dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lq782655835/blogs.git master:gh-pages
+git push -f git@github.com:lq782655835/blogs.git master
 
-# cd -
+cd -
