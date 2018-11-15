fee1191218869aac0cf9463b687a9838619e410d
liaoqiao@corp.netease.com <liaoqiao@corp.netease.com>
2018-11-15 23:04:18 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+# npm run docs:build
 
 # 进入生成的文件夹
-cd dist
+# cd dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -22,4 +22,4 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:lq782655835/blogs.git master
 
-cd -
+# cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,8 +8,15 @@ module.exports = {
     base: "/blogs/dist/",
     dest: "dist",
     themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Blogs', items: [
+          { text: 'Standard', link: '/team-standard/standard-ai-js.html' },
+          { text: 'Recommend', link: '/summary/Vue%E9%A1%B9%E7%9B%AE%E7%9B%AE%E5%BD%95%E7%BB%93%E6%9E%84%E6%8E%A8%E8%8D%90.html' }
+        ] },
+        { text: 'GitHub', link: 'https://github.com/lq782655835/blogs' },
+      ],
       sidebar: [
-        '/',
         {
           title: "团队规范",
           collapsable: false,
@@ -17,60 +24,53 @@ module.exports = {
             'team-standard/standard-ai-js',
             'team-standard/standard-ai-css',
             'team-standard/standard-ai-vue',
-            'team-standard/standard-ai-git',
-            // 'team-standard/stylelint样式规范工具'
+            'team-standard/standard-ai-git'
           ]
         },
-        // {
-        //   title: "经验总结",
-        //   collapsable: false,
-        //   children: [
-        //     'summary/Vue项目目录结构推荐',
-        //     'summary/Vue实例选项顺序推荐',
-        //     'summary/Vue-Router写法推荐',
-        //     'summary/200错误统一处理推荐',
-        //     'summary/for in和for of区别',
-        //     'summary/官网脚手架思考与实践',
-        //     'summary/Electron工程踩坑记录',
-        //     'summary/H5 Video踩坑记',
-        //     'tools/正则表达式一张图总结',
-        //     'tools/npm script技巧',
-        //     'tools/TypeScript开发Vue应用'
-        //   ]
-        // }
-        // {
-        //   title: "源码分析",
-        //   collapsable: false,
-        //   children: [
-        //     'code-analysis/Vue2.0源码分析 - 1. 框架结构',
-        //     'code-analysis/ES6-解构赋值及原理'
-        //   ]
-        // },
-        // {
-        //   title: "ReactNative",
-        //   collapsable: false,
-        //   children: [
-        //     'react-native/1. ReactNative环境搭建',
-        //     'react-native/2.ReactNative调试技巧',
-        //     'react-native/3.ReactNative使用Eslint检查代码规范'
-        //   ]
-        // },
-        // {
-        //   title: "其他",
-        //   collapsable: false,
-        //   children: [
-        //     '一个程序员的成长之路',
-        //     'tools/前端必备效率工具',
-        //     'tools/VSCode使用技巧',
-        //   ]
-        // }
+        {
+          title: "经验总结",
+          collapsable: true,
+          children: [
+            'summary/Vue项目目录结构推荐',
+            'summary/Vue实例选项顺序推荐',
+            'summary/Vue-Router写法推荐',
+            'summary/200错误统一处理推荐',
+            'summary/for in和for of区别',
+            'summary/官网脚手架思考与实践',
+            'summary/Electron工程踩坑记录',
+            'summary/H5 Video踩坑记',
+            'tools/正则表达式一张图总结',
+            'tools/npm script技巧',
+            'tools/TypeScript开发Vue应用'
+          ]
+        },
+        {
+          title: "源码分析",
+          collapsable: true,
+          children: [
+            'code-analysis/Vue2.0源码分析 - 1. 框架结构',
+            'code-analysis/ES6-解构赋值及原理'
+          ]
+        },
+        {
+          title: "ReactNative",
+          collapsable: true,
+          children: [
+            'react-native/1. ReactNative环境搭建',
+            'react-native/2.ReactNative调试技巧',
+            'react-native/3.ReactNative使用Eslint检查代码规范'
+          ]
+        },
+        {
+          title: "其他",
+          collapsable: true,
+          children: [
+            '一个程序员的成长之路',
+            'tools/前端必备效率工具',
+            'tools/VSCode使用技巧',
+          ]
+        }
       ],
       lastUpdated: '最后更新时间', // string | boolean
-      // displayAllHeaders: true,
-      // repo: 'https://g.hz.netease.com/ARfed/ai-fed-team-standard',
-      // repoLabel: '打开 Gitlab',
-      // docsDir: 'docs',
-      // editLinks: true,
-      // editLinkText: '编辑此页'
     }
   }
