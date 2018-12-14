7928df3416d60f6666635f9444c65742795f199c
springleo <782655835@qq.com>
2018-12-14 19:19:30 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lq782655835/blogs.git master
+git push -f https://github.com/lq782655835/blogs.git master
 
 # cd -
