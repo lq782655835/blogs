f7ea537692164701a8c86e7c0727db584a63f18d
lq782655835 <782655835@qq.com>
2020-05-08 14:15:08 +0800

    fix: chagned skin by vuesax

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,7 +19,7 @@ module.exports = {
         'flowchart',
         ['@vuepress/google-analytics', { ga: 'UA-154622307-1' }]
     ],
-    theme: 'antdocs',
+    theme: 'vuesax',
     themeConfig: {
         backToTop: true,
         sidebarDepth: 0,
@@ -34,8 +34,14 @@ module.exports = {
                 text: '工具',
                 items: [
                     { text: 'yiai-cli脚手架', link: 'https://github.com/lq782655835/yiai-cli' },
-                    { text: '可视化布局工具', link: 'https://github.com/lq782655835/vue-layout-preview' },
-                    { text: '缓存管理工具', link: 'https://github.com/lq782655835/cache-manage-js' },
+                    {
+                        text: '可视化布局工具',
+                        link: 'https://github.com/lq782655835/vue-layout-preview'
+                    },
+                    {
+                        text: '缓存管理工具',
+                        link: 'https://github.com/lq782655835/cache-manage-js'
+                    },
                     {
                         text: '自动生成blog目录工具',
                         link: 'https://github.com/lq782655835/node-vuepress-auto-read'
@@ -44,11 +50,14 @@ module.exports = {
                         text: 'url自动生成TypeScript类型',
                         link: 'https://github.com/lq782655835/json2ts'
                     },
-                    { text: '批量生成模板代码工具', link: 'https://github.com/lq782655835/auto-code' },
+                    {
+                        text: '批量生成模板代码工具',
+                        link: 'https://github.com/lq782655835/auto-code'
+                    },
                     {
                         text: 'svg转icon webpack插件',
                         link: 'https://github.com/lq782655835/svg-icon-webpack-plugin'
-                    },
+                    }
                 ]
             },
             {
@@ -56,18 +65,39 @@ module.exports = {
                 items: [
                     { text: 'yi-ui基础组件库', link: 'https://github.com/lq782655835/yi-ui' },
                     { text: 'vuele业务组件库', link: 'https://github.com/lq782655835/vuele' },
-                    { text: '基于el-table的配置化组件', link: 'https://github.com/lq782655835/el-table-plus' },
-                    { text: '基于el-form的配置化组件', link: 'https://github.com/lq782655835/el-form-plus' },
-                    { text: 'js api解耦dialog组件', link: 'https://github.com/lq782655835/el-dialog-helper' },
-                    { text: '常用工具函数库', link: 'https://github.com/lq782655835/30-seconds-of-code' },
+                    {
+                        text: '基于el-table的配置化组件',
+                        link: 'https://github.com/lq782655835/el-table-plus'
+                    },
+                    {
+                        text: '基于el-form的配置化组件',
+                        link: 'https://github.com/lq782655835/el-form-plus'
+                    },
+                    {
+                        text: 'js api解耦dialog组件',
+                        link: 'https://github.com/lq782655835/el-dialog-helper'
+                    },
+                    {
+                        text: '常用工具函数库',
+                        link: 'https://github.com/lq782655835/30-seconds-of-code'
+                    }
                 ]
             },
             {
                 text: 'vuepress插件',
                 items: [
-                    { text: 'vuepress Vue代码预览插件', link: 'https://github.com/lq782655835/vuepress-plugin-demo-block' },
-                    { text: 'vuepress element-ui扩展插件', link: 'https://github.com/lq782655835/vuepress-plugin-element-ui' },
-                    { text: 'vuepress sax主题插件', link: 'https://github.com/lq782655835/vuepress-theme-vuesax' },
+                    {
+                        text: 'vuepress Vue代码预览插件',
+                        link: 'https://github.com/lq782655835/vuepress-plugin-demo-block'
+                    },
+                    {
+                        text: 'vuepress element-ui扩展插件',
+                        link: 'https://github.com/lq782655835/vuepress-plugin-element-ui'
+                    },
+                    {
+                        text: 'vuepress sax主题插件',
+                        link: 'https://github.com/lq782655835/vuepress-theme-vuesax'
+                    }
                 ]
             },
             {
